8174b18dd4cfc9adb044b8ddff90d8e91a990af1
Kian van der Meer <kiansinan@gmail.com>
2021-04-23 17:20:03 +0200

    bla

--- App.js
+++ App.js
@@ -3,11 +3,35 @@ import React from 'react';
 import { StyleSheet, Text, View, TouchableHighlight, ScrollView, Button, Dimensions, useEffect } from 'react-native';
 
 export default function App() {
+    let [token, setToken] = React.useState('');
     let [discussionpostsArray, setDiscussionpostsArray] = React.useState([]);
     
-    let token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MTkwODIwMzgsImV4cCI6MTYxOTA4NTYzOCwiaXNzIjoiaHR0cDpcL1wvbG9jYWxob3N0XC9hcGlcLyIsImRhdGEiOnsiaWQiOiJ1bmRlZmluZWQiLCJ1c2VybmFtZSI6InVuZGVmaW5lZCIsInNjaG9vbCI6InB1YmxpYyJ9fQ.NinxQ7Zh0utQOX4FL_J7ryLXrzl_iysYGEqezYB6rJg";
+    async function loginAsGuest() {
+        try {
+            let response = await fetch('http://localhost/login.php', {
+                method: 'POST',
+                headers: {
+                  Accept: 'application/json',
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                  guest: true
+                })
+              });
+            let responseJson = await response.json();
+            console.log(responseJson);
+            setToken(responseJson.token);
+        }
+        catch (error) {
+            console.error(error);
+        }
+    }       
     async function getData() {
         try {
+            if (!token) {
+                loginAsGuest();
+                console.log(token);
+            }
             let response = await fetch(
                 'http://localhost/api/get_posts.php?token='+token,
             );
@@ -20,26 +44,39 @@ export default function App() {
             console.error(error);
         }
     }
+    function ShowPosts() {
+        if (discussionpostsArray.length > 0) {
+            discussionpostsArray.map((post, index) => {
 
+                return (
+                    <View key={index} style={styles.discussionpostWrapper}>
+                        <View key={'post'+index.toString()} style={styles.discussionpost}>
+                            <Text key={'title'+index.toString()} style={styles.discussionpostTitle}>{post.data.discussionpostTitle}</Text>
+                            <Text key={'break'+index.toString()}> </Text>
+                            <Text key={'body'+index.toString()} style={styles.discussionpostBody}>{post.data.discussionpostBody}</Text>
+                            <Text key={'link'+index.toString()} style={{color:'red'}}>read more</Text>
+                        </View>
+                    </View>
+                );
+            });
+        }
+        else {
+            return (
+                <View style={styles.discussionpostWrapper}>
+                    <View style={styles.discussionpost}>
+                        <Text style={styles.discussionpostTitle}>No posts available.</Text>
+                    </View>
+                </View>
+            );
+        }
+    }
     return (
         <View style={styles.container}>
 
             <Text style={styles.header} onPress={getData}>Discuss My School</Text>
             
             <ScrollView>
-                {discussionpostsArray.map((post, index) => {
-                    
-                    return (
-                        <View key={index} style={styles.discussionpostWrapper}>
-                            <View key={'post'+index.toString()} style={styles.discussionpost}>
-                                <Text key={'title'+index.toString()} style={styles.discussionpostTitle}>{post.data.discussionpostTitle}</Text>
-                                <Text key={'break'+index.toString()}> </Text>
-                                <Text key={'body'+index.toString()} style={styles.discussionpostBody}>{post.data.discussionpostBody}</Text>
-                                <Text key={'link'+index.toString()} style={{color:'red'}}>read more</Text>
-                            </View>
-                        </View>
-                    );
-                })}
+                <ShowPosts></ShowPosts>
             </ScrollView>
         </View>
         );
@@ -72,9 +109,9 @@ const styles = StyleSheet.create({
     },
     header: {
         color: 'red',
-        marginTop: 10,
+        marginTop: 40,
         marginBottom: 10,
-        fontSize: 'x-large'
+        fontSize: 30
     },
     discussionpostTitle: {
         color: 'white',
