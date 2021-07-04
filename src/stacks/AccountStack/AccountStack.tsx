bf8b956b8c42f083ef11c5577f17df5c4088dc54
Kian van der Meer <kiansinan@gmail.com>
2021-07-04 08:08:14 +0200

    organzid imports

--- src/stacks/AccountStack/AccountStack.tsx
+++ src/stacks/AccountStack/AccountStack.tsx
@@ -1,7 +1,5 @@
-import { getFocusedRouteNameFromRoute } from "@react-navigation/native";
-import { createStackNavigator, StackNavigationOptions } from "@react-navigation/stack";
-import React, { useEffect } from "react";
-import { SubmitButton } from "../../objects/Form/SubmitButton";
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
 import { LoginStackOptions } from "../../options/LoginStackOptions";
 import { RegisterStackOptions } from "../../options/RegisterStackOptions";
 import { LoginStack } from "./LoginStack/LoginStack";
