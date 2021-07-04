f2b41f6e2b091dbf6b1010e4e6b99a9e06ee7b8e
Kian van der Meer <kiansinan@gmail.com>
2021-07-04 15:29:04 +0200

    removed notes

--- src/App.tsx
+++ src/App.tsx
@@ -50,58 +50,6 @@ const TabBarOptions: BottomTabBarOptions = {
 // bundesländer sortieren
 // bilder für noten
 
-// if not logged in, you wont get in the app
-/*
-          Social School
-
-           posttitle
-    asdasdkfhdsjkhgjksdhfhsd
-    djahdjkashjdkhasjdhahdjkha
-   
-            posttitle
-    asdasdkfhdsjkhgjksdhfhsd
-    djahdjkashjdkhasjdhahdjkha
-
-            posttitle
-    asdasdkfhdsjkhgjksdhfhsd
-    djahdjkashjdkhasjdhahdjkha
-
-
-<usericon> <feedicon> <gradesicon>    (accountstack)
-  user        feed        grades
-______________________________________
-on userstack:
-  -> show profile screen
-    (instagram like)
-    -> edit profile button (navigates to edit)
-    -> logout button
-  -> edit profile screen
-  -> tab bar at bottom
-on feedstack:
-  -> feedscreen
-    -> create button
-    -> show posts
-  -> poststack
-    -> postscreen with comments
-      -> edit button if owner of post
-    -> edit post screen
-  -> tab bar at bottom
-on gradestack:
-  -> grade summary screen
-  -> grades of subject summary screen
-  -> one grade screen
-on accountstack:
-  -> loginstack
-    -> enter username
-    -> enter password
-    -> enter school
-  -> registerstack
-    -> enter username
-    -> enter password
-    -> confirm password
-    -> enter school
-*/
-
 const Content = () => {
   const [authenticated, setAuthenticated] = useState(false);
   const [{data, fetching, error}] = useMe();
