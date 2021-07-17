7af8a6c550dfa4b4c75858679fd09bb12f7df7fc
Kian van der Meer <kiansinan@gmail.com>
2021-07-17 18:03:21 +0200

    fixed margintop

---
+++ src/stacks/FeedStack/PostStack/Post/CreatePostStack/CreatePostStack.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { createStackNavigator, StackNavigationOptions } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+
+export const CreatePostStack = ({ route }: any) => {
+
+  return (
+    <Stack.Navigator>
+      <Stack.Screen component={}/>
+    </Stack.Navigator>
+  );
+};
