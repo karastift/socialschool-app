c0db0b34f2a663482bd278327a43af63ffe1f350
kara <kiansinan@gmail.com>
2021-08-05 11:02:32 +0200

    reducer method instead of map

--- src/utils/getAverage.ts
+++ src/utils/getAverage.ts
@@ -2,14 +2,8 @@ import { GradeTypes } from "../types/GradeTypes";
 
 export const getAverage = (array: GradeTypes[]) => {
   
-  let val = array[0].value;
-
-  if (array.every(({ value }) => value === val)) {
-    let sum = 0;
-    array.map((grade) => {
-      sum += grade.grade;
-    });
-
+  if (array.every(({ value }) => value === array[0].value)) {
+    const sum = array.reduce((sum, curr) => sum + curr.grade, 0);
     return (sum / array.length).toFixed(2);
   }
 
@@ -20,13 +14,13 @@ export const getAverage = (array: GradeTypes[]) => {
 
   let gradesByValues: any = {};
 
-  for (val of values) {
+  for (let val of values) {
     gradesByValues[val] = array.filter(({ value }) => value === val);
   }
   let average = 0;
 
   for (let val of Object.keys(gradesByValues)) {
-    let sum = gradesByValues[val].reduce((a: number, { grade }: any) => a + grade, 0);
+    let sum = gradesByValues[val].reduce((a: number, { grade }: GradeTypes) => a + grade, 0);
     average += (sum / gradesByValues[val].length) * parseFloat(val);
   }
   return average.toFixed(2);
